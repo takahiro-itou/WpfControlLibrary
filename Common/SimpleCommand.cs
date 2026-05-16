25ff85ddba79b391614cb568d308c68c98d5262c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-05-16 10:58:37 +0900

    クラス SimpleCommand を新規作成

---
+++ Common/SimpleCommand.cs
@@ -0,0 +1,52 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+using System.ComponentModel;
+using System.Windows.Input;
+
+
+namespace WpfControl.Common
+{
+
+//========================================================================
+//
+//    SimpleCommand  class.
+//
+
+public  class  SimpleCommand : ICommand
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//========================================================================
+//
+//    Public Member Functions (Implement Interface).
+//
+
+//========================================================================
+//
+//    Public Properties (Implement Interface).
+//
+
+//========================================================================
+//
+//    Member Variables.
+//
+
+}   //  End class AbstractSampleViewModel
+
+}   //  End of namespace  WpfControl.Sample
