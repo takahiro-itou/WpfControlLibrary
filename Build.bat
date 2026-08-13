972f8a3047dc5223fde58b7aa1d838419c2a8d3f
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-13 10:09:19 +0900

    リファクタリング

--- Build.bat
+++ Build.bat
@@ -1,8 +1,9 @@
 
 set  solution=WpfControl
+set  target=Build
 set  config="Debug"
 
 
-msbuild  -restore  -t:Build     ^
+msbuild  -restore  -t:%target%  ^
     -p:Configuration=%config%   -p:Platform=x64     ^
     "%solution%.sln"
