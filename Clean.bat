b5363e38bbadfa2d7fc8f1c0a6dcf8743d8a4d20
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-08-13 09:49:44 +0900

    ビルド用バッチファイルを作成

---
+++ Clean.bat
@@ -0,0 +1,8 @@
+
+set  solution=WpfControl
+set  config="Debug"
+
+
+msbuild  -restore  -t:Clean     ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.sln"
---
+++ Rebuild.NetOld.bat
@@ -0,0 +1,12 @@
+
+set  solution=WpfControl
+set  config="Debug"
+
+
+msbuild  -restore  -t:Clean     ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.NetOld.sln"
+
+msbuild  -restore  -t:Rebuild   ^
+    -p:Configuration=%config%   -p:Platform=x64     ^
+    "%solution%.NetOld.sln"
