dd6baf21e1835dcd410de459e4430d990269522c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2026-09-05 11:02:48 +0900

    空のクラスを新規作成

---
+++ Common/AbstractScrollInfo.cs
@@ -0,0 +1,50 @@
+﻿//  -*-  coding: utf-8-with-signature  -*-  //
+/*************************************************************************
+**                                                                      **
+**                  ---  WPF UserControl Library.  ---                  **
+**                                                                      **
+**          Copyright (C), 2026-2026, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+
+namespace  WpfControl.Common  {
+
+
+//========================================================================
+//
+//    AbstractScrollInfo  class.
+//
+
+public  class  AbstractScrollInfo : IScrollInfo
+{
+
+//========================================================================
+//
+//    Constructor(s) and Destructor.
+//
+
+//----------------------------------------------------------------
+/**   コンストラクタ。
+**
+**/
+
+public
+AbstractScrollInfo(
+{
+}
+
+
+//========================================================================
+//
+//    Public Member Functions (Implement Interface).
+//
+
+}   //  End class  AbstractScrollInfo
+
+}   //  End of namespace  WpfControl.Common
